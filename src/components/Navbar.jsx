2a143586ff3f6d2cdc358d8ab15b64462ef7b8e6
yyds <yyds@gmail.com>
2024-08-15 14:34:11 +0800

    navbar & menu finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,116 @@
-import { AppBar, styled, Toolbar, Typography , Box, InputBase, Badge, Divider} from "@mui/material";
+import {
+  AppBar,
+  styled,
+  Toolbar,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+  Divider,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import React from "react";
-import PetsIcon from '@mui/icons-material/Pets';
-// import MailIcon from '@mui/icons-material/Mail';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-// import { theme } from "../theme";
+import PetsIcon from "@mui/icons-material/Pets";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
-  display: 'flex',
-  justifyContent: 'space-between'
+  display: "flex",
+  justifyContent: "space-between",
 });
 
-const Search = styled("div")(({theme}) => ({
-  background: 'white',
+const Search = styled("div")(({ theme }) => ({
+  background: "white",
   padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
-  width: '40%'
+  width: "40%",
 }));
 
-const Icons = styled(Box)(({theme}) => ({
+const Icons = styled(Box)(({ theme }) => ({
   // backgroundColor: 'white'
-  display: 'flex',
-  justifyContent: 'space-between',
-  
+  display: "none",
+  justifyContent: "space-between",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  }, //
+}));
 
-}))
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null)
+
+const handleClick = (e) => {
+  const {currentTarget}  = e
+  setOpen(true)
+  setAnchorEl(currentTarget)
+} 
+
+const handleClose = e => {
+  setOpen(false)
+  console.log("menu closed")
+}
   return (
     <AppBar position="stiky">
       <StyledToolbar>
-        <Typography variant="h6" sx={{display:{xs: 'none', sm: 'block'}}}>Lama Dev</Typography>
-        <PetsIcon sx={{display:{xs: 'block', sm: 'none'}}} />
-        <Search> 
-          <InputBase placeholder="search"/>
-         </Search>
-        <Icons>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Lama Dev
+        </Typography>
+        <PetsIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search" />
+        </Search>
+        <Icons id="positioned-icons">
           <Badge badgeContent={4} color="error">
-            <MailIcon ></MailIcon>
+            <MailIcon></MailIcon>
           </Badge>
-          <Divider sx={{marginLeft: '10px', marginRight: '6px'}} orientation="vertical" variant="middle" flexItem/>
+          {/* <Divider sx={{marginLeft: '10px', marginRight: '6px'}} orientation="vertical" variant="middle" flexItem/> */}
           <Badge badgeContent={2} color="error">
             <NotificationsIcon />
           </Badge>
-
+          <Avatar
+            id="fade-button"
+            onClick={handleClick}
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
         </Icons>
+        <UserBox onClick={handleClick}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <Typography variant="span">John</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu id="fade-menu" MenuListProps={{ 'aria-labelledby': 'fade-button'}} open={open} anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right'
+      }} transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }} 
+      onClose={handleClose}
+      anchorEl={anchorEl}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
